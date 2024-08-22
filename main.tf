68f75b6efaecf93a060d56dff0e517deffbf3c2e
gabrielBmoraes <gabriel.moraes@sptech.school>
2024-08-21 21:22:37 -0300

    teste 1

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2-terraform-2" {
+  ami = "ami-0e86e20dae9224db8"
+  instance_type = "t2.small"
+  tags = {
+    Name = "ec2-terraform-2"
+  }
+}
