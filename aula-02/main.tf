f55767e1020aa90e2779fa1e4aaaf14edebd8f58
gabrielBmoraes <gabriel.moraes@sptech.school>
2024-08-28 20:44:59 -0300

    crianco sub rede

--- aula-02/main.tf
+++ aula-02/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "ec2-iac-aula2" {
   ami= "ami-0e86e20dae9224db8"
   instance_type = "t2.small"
   tags = {
-    Name = "ec2-iac-aula2"
+    Name = "ec2-iac-aula2-01"
   }
 
   ebs_block_device {
@@ -25,4 +25,32 @@ resource "aws_instance" "ec2-iac-aula2" {
     volume_size = 30
     volume_type = "gp3"
   }
+
+  security_groups =  [aws_security_group.sg_aula_iac.name, "default"]
+
+  key_name = "aua_iac"
+
+  subnet_id = "subnet-0823e4ed0fda0922a" // id de vpc existente
+}
+
+variable "porta_https" {
+  description = "porta https"
+  default = 80
+  type = number
+}
+
+resource "aws_security_group" "sg_aula_iac" {
+  name = "sg_aula_iac"
+
+  ingress {
+    from_port = var.porta_https
+    to_port = var.porta_https
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_subnet" "minha_subrede" {
+  vpc_id = "id da vpc"
+  cidr_block = "10.10.10.0/24"
 }
